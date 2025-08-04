dc7a6ba063c90df3bc939dcee8961f4c20787060
ravinwoenarsooo <127296688+ravinwoenarsooo@users.noreply.github.com>
2025-08-04 14:09:55 +0700

    [FE] Employee Assign Division Page #31
    -: fix header to display correct company name

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -33,7 +33,7 @@ export function Header() {
   const toggleSidebarCollapse = useGeneralStore((s) => s.toggleSidebarCollapse);
   const { isFetchingGetMe } = user.useGetMe();
 
-  company.useGetCompanyByUuid(userInfo.uuid ?? "");
+  company.useGetCurrentCompanyByUserUuid(userInfo.uuid ?? "");
 
   return (
     <header className="flex items-center justify-between px-6 py-4 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full">
